2fac2b4aeddb977da7c1f9e3901faffdd1ab2983
srihari <hsri0007@gmail.com>
2023-09-26 19:33:03 +0530

    serialize with json parse

--- src/App.js
+++ src/App.js
@@ -1,42 +1,49 @@
-import './App.css';
-import { useEffect, useState, memo } from 'react';
+import "./App.css";
+import { useEffect, useState, memo } from "react";
 
+function useLocalStorage(
+  key,
+  defaultValue = '',
+  { serialize = JSON.stringify, deSerialize = JSON.parse } = {}
+) {
+  const [state, setState] = useState(() => {
+    const valueFromLocal = window.localStorage.getItem(key);
+    if (valueFromLocal) {
+      return deSerialize(valueFromLocal);
+    }
+    return defaultValue;
+  }); // async
 
-
-
-function useLocalStorage (key, defaultValue=''){
-  const [state, setState] = useState(()=>window.localStorage.getItem(key) || defaultValue); // async
-
-
-  useEffect(()=>{
-    window.localStorage.setItem(key, state)
-  },[state, key])//DEPENDENCY
-
+  useEffect(() => {
+    window.localStorage.setItem(key, serialize(state));
+  }, [state, key]); //DEPENDENCY
 
   return [state, setState];
-
 }
 
-
-const MemoGreeting = memo(function Greeting({initial = ''}){
-  const [name, setName] = useLocalStorage('name', initial);
+const MemoGreeting = memo(function Greeting({ initial = "" }) {
+  const [name, setName] = useLocalStorage("name", initial);
 
   return (
     <div className="App">
-      <input type='text' value={name} onChange={(e)=>setName(e.target.value)} />
-     <h1>{name}</h1>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <h1>{name}</h1>
     </div>
-  ); 
-})
+  );
+});
 
 function App() {
-  const [count, setCount] =useState(0);
+  const [count, setCount] = useState(0);
   return (
     <div>
-      <MemoGreeting initial='sdfs' />
-      <button onClick={()=>setCount(count+1)} >{count}</button>
+      <MemoGreeting initial="sdfs" />
+      <button onClick={() => setCount(count + 1)}>{count}</button>
     </div>
-  )
+  );
 }
 
 export default App;
