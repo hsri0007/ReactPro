8db23108c404b60f7455463c9db0335d1ebcfee2
srihari <hsri0007@gmail.com>
2023-09-27 21:36:07 +0530

    rendering process

--- src/App.js
+++ src/App.js
@@ -1,19 +1,53 @@
-import './App.css';
-import { useState } from 'react';
+
+import { useEffect, useLayoutEffect, useState } from 'react';
 
 
-function Greeting({initial}){
-  const [name, setName] = useState(initial);
+function Greeting(){
+  const [name, setName] = useState(()=>{
+    console.log(" child im renderring");
+    return ''
+  });
+  useEffect(()=>{
+    console.log("child im renderring useEffect")
+    return ()=>{
+      console.log("child cleanup im renderring useEffect")
+    }
+  },[])
+  useLayoutEffect(()=>{
+    console.log("child im renderring Layout useEffect")
+  },[])
+  console.log(" child im renderring out")
   return (
     <div className="App">
       <input type='text' value={name} onChange={(e)=>setName(e.target.value)} />
      <h1>{name}</h1>
     </div>
-  ); 
+  );
 }
 
 function App() {
-  return <Greeting initial='hello' />
+  const [name, setName] = useState(()=>{
+    console.log("  im renderring");
+    return ''
+  });
+  useEffect(()=>{
+    console.log(" im renderring useEffect")
+       return ()=>{
+      console.log(" cleanup im renderring useEffect")
+    }
+  },[])
+
+  useLayoutEffect(()=>{
+    console.log(" im renderring Layout useEffect")
+  },[])
+  console.log("  im renderring out")
+  return (
+    <div className="App">
+      <input type='text' value={name} onChange={(e)=>setName(e.target.value)} />
+     {name}
+     <Greeting />
+    </div>
+  ); 
 }
 
 export default App;
